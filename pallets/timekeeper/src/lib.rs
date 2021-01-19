36014c0d1da83e0d3a777b7355c585732bf144d8
Andy Bell <andy.bell@barkingmad.io>
2021-01-19 21:20:28 +0100

    add rates and administrators

--- pallets/timekeeper/src/lib.rs
+++ pallets/timekeeper/src/lib.rs
@@ -9,29 +9,26 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
-/// Configure the pallet by specifying the parameters and types on which it depends.
+type Value = u32;
+
 pub trait Trait: frame_system::Trait {
-	/// Because this pallet emits events, it depends on the runtime's definition of an event.
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
-// The pallet's runtime storage items.
-// https://substrate.dev/docs/en/knowledgebase/runtime/storage
 decl_storage! {
 	trait Store for Module<T: Trait> as TimeKeeperModule {
-		// Learn more about declaring storage items:
-		// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
-		Something get(fn something): Option<u32>;
+		/// Store the rate for an account
+		pub Rates get(fn rates): map hasher(blake2_128_concat) T::AccountId => Option<Value>;
+		/// Store a whitelist of administrators
+		pub Administrators get(fn adminstrators): map hasher(blake2_128_concat) T::AccountId => Option<bool>;		
 	}
 }
 
-// Pallets use events to inform users when important changes are made.
-// https://substrate.dev/docs/en/knowledgebase/runtime/events
 decl_event!(
 	pub enum Event<T> where AccountId = <T as frame_system::Trait>::AccountId {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, AccountId),
+		/// An account has been registered with an hourly rate
+		/// [account, value]
+		AccountRegistered(AccountId, Option<Value>),
 	}
 );
 
@@ -45,31 +42,19 @@ decl_error! {
 	}
 }
 
-// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-// These functions materialize as "extrinsics", which are often compared to transactions.
-// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 decl_module! {
 	pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		// Errors must be initialized if they are used by the pallet.
 		type Error = Error<T>;
 
-		// Events must be initialized if they are used by the pallet.
 		fn deposit_event() = default;
 
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[weight = 10_000 + T::DbWeight::get().writes(1)]
-		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+		pub fn register_account(origin, account: T::AccountId, value: Option<Value>) -> dispatch::DispatchResult {
 			let who = ensure_signed(origin)?;
-
-			// Update storage.
-			Something::put(something);
-
+			// TODO check if the origin is in the whitelist
+			Rates::<T>::mutate_exists(&account, |v| *v = value);
 			// Emit an event.
-			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			Self::deposit_event(RawEvent::AccountRegistered(account, value));
 			// Return a successful DispatchResult
 			Ok(())
 		}
@@ -80,17 +65,19 @@ decl_module! {
 			let _who = ensure_signed(origin)?;
 
 			// Read a value from storage.
-			match Something::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					Something::put(new);
-					Ok(())
-				},
-			}
+			// match Something::get() {
+			// 	// Return an error if the value has not been set.
+			// 	None => Err(Error::<T>::NoneValue)?,
+			// 	Some(old) => {
+			// 		// Increment the value read from storage; will error in the event of overflow.
+			// 		let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+			// 		// Update the value in storage with the incremented result.
+			// 		Something::put(new);
+			// 		Ok(())
+			// 	},
+			// }
+
+			Ok(())
 		}
 	}
 }
